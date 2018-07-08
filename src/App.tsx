ad3a1d60b3f2100e109522084463122414b12dc1
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-07-08 11:50:39 +0530

    Fixed the landing page

--- src/App.tsx
+++ src/App.tsx
@@ -3,52 +3,63 @@ import { ThemeProvider, Text, Section, Heading } from '@hackclub/design-system';
 import AppLogo from './components/AppLogo';
 import logo from './logo.svg';
 import { Parallax } from 'react-parallax';
-const image1 = '/iphone.jpeg';
+const image1 =
+  'https://images.unsplash.com/photo-1510166089176-b57564a542b1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=befea110acdbc00f7b94c872cf76f37b';
 class App extends React.Component {
   public render() {
     return (
       <ThemeProvider webfonts={true}>
-        <div style={{ height: 500 }}>
-          <Parallax bgImage={image1} strength={500} style={{ height: 500 }}>
-            <div style={{ height: 500 }}>
-              {/*         <div
-                style={{
-                  background: 'transparent',
-                  padding: 20,
-                  position: 'absolute',
-                  top: '20%',
-                  left: '50%',
-                  fontWeight: 'bold',
-                  transform: 'translate(-50%,-50%)'
-                }}
-              >
-                HTML inside the parallax
-              </div> */}
-              <Heading.h3
-                color={'black'}
-                style={{
-                  background: 'transparent',
-                  padding: 20,
-                  position: 'absolute',
-                  top: '20%',
-                  left: '50%',
-                  fontWeight: 'bold',
-                  transform: 'translate(-50%,-50%)'
-                }}
-              >
-                Welcome to React
-              </Heading.h3>
-            </div>
-          </Parallax>
-        </div>
-        <Section bg="black">
+        <Parallax bgImage={image1} strength={500}>
+          <div style={{ height: 500 }}>
+            <AppLogo src={logo} alt="logo" />
+            <Heading.h2
+              color={'black'}
+              style={{
+                background: 'transparent',
+                padding: 20,
+                position: 'absolute',
+                top: '25%',
+                left: '50%',
+                fontWeight: 'bold',
+                transform: 'translate(-50%,-50%)'
+              }}
+            >
+              React Shop
+            </Heading.h2>
+          </div>
+        </Parallax>
+        {/*         <Section bg="black">
           <AppLogo src={logo} alt="logo" />
           <Heading.h3>Welcome to React</Heading.h3>
-        </Section>
+        </Section> */}
         <Text m={3} align="center">
           To get started, edit <code>src/App.js</code> and save to reload.
         </Text>
-        <Section bg="accent">
+        <Section bg="white">
+          <Text m={3} align="center">
+            Hello
+          </Text>
+          <Text m={3} align="center">
+            Hello
+          </Text>
+        </Section>
+        <Section bg="white">
+          <Text m={3} align="center">
+            Hello
+          </Text>
+          <Text m={3} align="center">
+            Hello
+          </Text>
+        </Section>
+        <Section bg="white">
+          <Text m={3} align="center">
+            Hello
+          </Text>
+          <Text m={3} align="center">
+            Hello
+          </Text>
+        </Section>
+        <Section bg="white">
           <Text m={3} align="center">
             Hello
           </Text>
--- src/components/AppLogo.tsx
+++ src/components/AppLogo.tsx
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 const AppLogo = styled.img`
   animation: App-logo-spin infinite 20s linear;
   height: 80px;
+  display: block;
+  top: '5%';
+  margin-left: auto;
+  margin-right: auto;
   @keyframes App-logo-spin {
     from {
       transform: rotate(0deg);
