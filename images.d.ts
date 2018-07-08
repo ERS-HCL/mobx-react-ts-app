217d2685e23014ffe5f3000b189ab8bb1f75029e
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-07-08 09:01:07 +0530

    Added parallax

--- images.d.ts
+++ images.d.ts
@@ -2,3 +2,4 @@ declare module '*.svg';
 declare module '*.png';
 declare module '*.jpg';
 declare module '@hackclub/design-system';
+declare module 'react-parallax';
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
 import * as React from 'react';
 import { ThemeProvider, Text, Section, Heading } from '@hackclub/design-system';
-import AppLogo from './AppLogo';
+import AppLogo from './components/AppLogo';
 import logo from './logo.svg';
-
+import { Parallax } from 'react-parallax';
+const image1 = '/iphone.jpeg';
 class App extends React.Component {
   public render() {
     return (
       <ThemeProvider webfonts={true}>
+        <div style={{ height: 500 }}>
+          <Parallax bgImage={image1} strength={500} style={{ height: 500 }}>
+            <div style={{ height: 500 }}>
+              {/*         <div
+                style={{
+                  background: 'transparent',
+                  padding: 20,
+                  position: 'absolute',
+                  top: '20%',
+                  left: '50%',
+                  fontWeight: 'bold',
+                  transform: 'translate(-50%,-50%)'
+                }}
+              >
+                HTML inside the parallax
+              </div> */}
+              <Heading.h3
+                color={'black'}
+                style={{
+                  background: 'transparent',
+                  padding: 20,
+                  position: 'absolute',
+                  top: '20%',
+                  left: '50%',
+                  fontWeight: 'bold',
+                  transform: 'translate(-50%,-50%)'
+                }}
+              >
+                Welcome to React
+              </Heading.h3>
+            </div>
+          </Parallax>
+        </div>
         <Section bg="black">
           <AppLogo src={logo} alt="logo" />
           <Heading.h3>Welcome to React</Heading.h3>
@@ -14,6 +48,14 @@ class App extends React.Component {
         <Text m={3} align="center">
           To get started, edit <code>src/App.js</code> and save to reload.
         </Text>
+        <Section bg="accent">
+          <Text m={3} align="center">
+            Hello
+          </Text>
+          <Text m={3} align="center">
+            Hello
+          </Text>
+        </Section>
       </ThemeProvider>
     );
   }
