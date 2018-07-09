48d48dc35459eab5dcfaac190fea17390f035013
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-07-09 14:20:59 +0530

    Added form

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import * as React from 'react';
-import {
-  ThemeProvider,
-  Text,
-  Section,
-  Card,
-  Flex,
-  Box,
-  Button,
-  Label,
-  Field
-} from '@hackclub/design-system';
+import { ThemeProvider, Flex, Box } from '@hackclub/design-system';
 import AppHero from './components/AppHero/AppHero';
+import StepOne from './components/StepOne/stepOne';
 const image1 =
   'https://images.unsplash.com/photo-1510166089176-b57564a542b1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=befea110acdbc00f7b94c872cf76f37b';
 class App extends React.Component {
+  handleSubmit = (data: any) => {
+    console.log(data);
+  };
+
   public render() {
     return (
       <ThemeProvider webfonts={true}>
@@ -23,28 +18,12 @@ class App extends React.Component {
           text={'React Shop'}
           image={image1}
         />
-        <Flex justify="space-around">
-          <Card
-            boxShadowSize="md"
-            my={4}
-            p={3}
-            width={'90%'}
-            color="black"
-            bg="white"
-            borderRadius={8}
-          >
-            <Flex align="center">
-              <Label m={3} htmlFor="sample-input">
-                Name
-              </Label>
-              <Field name="fname" placeholder="Zach" />
-            </Flex>
-            <Flex justify="flex-end">
-              <Button>Button</Button>
-            </Flex>
-          </Card>
+        <Flex bg="smoke">
+          <Box px={3} width={1}>
+            <StepOne eventHandler={this.handleSubmit} />
+          </Box>
         </Flex>
-        <Box p={3} color="white" bg="accent" boxShadowSize="md" my={4}>
+        {/*        <Box p={3} color="white" bg="accent" boxShadowSize="md" my={4}>
           Hello
         </Box>
         <Text m={3} align="center">
@@ -81,7 +60,7 @@ class App extends React.Component {
           <Text m={3} align="center">
             Hello
           </Text>
-        </Section>
+        </Section> */}
       </ThemeProvider>
     );
   }
---
+++ src/components/StepOne/stepOne.tsx
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import { Flex, Button, Field, Card } from '@hackclub/design-system';
+
+interface IStepOneProps {
+  eventHandler: any;
+}
+
+interface IStepOneState {
+  email: string;
+  fname: string;
+  lname: string;
+}
+
+class StepOne extends React.Component<IStepOneProps, IStepOneState> {
+  state: IStepOneState = {
+    fname: '',
+    email: '',
+    lname: ''
+  };
+
+  handleChange = (e: any) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value } as any);
+  };
+
+  handleClose = () => {
+    const { eventHandler } = this.props;
+    eventHandler(this.state);
+  };
+
+  public render() {
+    const { fname, lname, email } = this.state;
+    return (
+      <Flex justify="space-around">
+        <Card
+          boxShadowSize="md"
+          my={4}
+          p={3}
+          width={'90%'}
+          color="black"
+          bg="white"
+          borderRadius={8}
+        >
+          <Field
+            name="fname"
+            placeholder="First Name"
+            label="First Name"
+            error=""
+            defaultValue={fname}
+            onChange={this.handleChange}
+          />
+          <Field
+            name="lname"
+            placeholder="Last Name"
+            label="Last Name"
+            error=""
+            defaultValue={lname}
+            onChange={this.handleChange}
+          />
+          <Field
+            name="email"
+            placeholder="Email"
+            type="email"
+            label="Email"
+            error=""
+            defaultValue={email}
+            onChange={this.handleChange}
+          />
+          <Flex justify="flex-end">
+            <Button onClick={this.handleClose}>SAVE</Button>
+          </Flex>
+        </Card>
+      </Flex>
+
+      /*       <div>
+        <div>{text}</div>
+        <input name="name" value={name} onChange={this.handleChange} />
+      </div> */
+    );
+  }
+}
+
+export default StepOne;
