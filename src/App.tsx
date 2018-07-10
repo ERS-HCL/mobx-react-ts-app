ee7b50572e23c68217fc8c66302c3eb5f11467e0
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-07-10 10:44:07 +0530

    wired up the store and downgraded to mobx 4 for IE support

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,21 @@ import { ThemeProvider } from '@hackclub/design-system';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import AppMain from './components/AppMain/AppMain';
 import ThankYou from './components/ThankYou/ThankYou';
+import { Provider } from 'mobx-react';
+import shopStore from './stores/shopStore';
 
 class App extends React.Component {
   public render() {
     return (
-      <ThemeProvider webfonts={true}>
-        <Switch>
-          <Route path="/thankyou" component={ThankYou} />
-          <Route path="/" exact={true} component={AppMain} />
-          <Redirect to="/" />
-        </Switch>
-      </ThemeProvider>
+      <Provider shopStore={shopStore}>
+        <ThemeProvider webfonts={true}>
+          <Switch>
+            <Route path="/thankyou" component={ThankYou} />
+            <Route path="/" exact={true} component={AppMain} />
+            <Redirect to="/" />
+          </Switch>
+        </ThemeProvider>
+      </Provider>
     );
   }
 }
--- src/components/AppMain/AppMain.tsx
+++ src/components/AppMain/AppMain.tsx
@@ -3,9 +3,28 @@ import AppHero from '../AppHero/AppHero';
 import StepOne from '../StepOne/stepOne';
 import { Flex, Box } from '@hackclub/design-system';
 import { Redirect } from 'react-router-dom';
+import { observer, inject } from 'mobx-react';
+import { ShopStore } from '../../stores/shopStore';
+import { IProduct } from '../../models/product';
+
 const image1 =
   'https://images.unsplash.com/photo-1510166089176-b57564a542b1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=befea110acdbc00f7b94c872cf76f37b';
-class AppMain extends React.Component {
+
+interface IAppMainProps {
+  /**
+   * The store reference for the shop store.  Note that this needs to be optional
+   * because the <Provider> component adjusts things appropriately, which the
+   * code checker won't pick up on.
+   *
+   * @type {ShopStore}
+   * @memberof AppMainProps
+   */
+  shopStore?: ShopStore;
+}
+
+@inject('shopStore')
+@observer
+class AppMain extends React.Component<IAppMainProps> {
   state = {
     checkedOut: false
   };
@@ -16,8 +35,19 @@ class AppMain extends React.Component {
     });
   };
 
+  getProductList = (): IProduct[] => {
+    if (typeof this.props.shopStore !== 'undefined') {
+      return this.props.shopStore.products;
+    } else {
+      return [];
+    }
+  };
   public render() {
     const { checkedOut } = this.state;
+    const productList = this.getProductList().map(product => (
+      <div key={product.name}>{product.name}</div>
+    ));
+
     return (
       <div>
         {checkedOut && <Redirect to="/thankyou" />}
@@ -27,6 +57,7 @@ class AppMain extends React.Component {
           text={'React Shop'}
           image={image1}
         />
+        {productList}
         <Flex bg="smoke">
           <Box px={3} width={1}>
             <StepOne eventHandler={this.handleSubmit} />
